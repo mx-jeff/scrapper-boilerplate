b463e3e9a0a10664f1a355cbd5954caa9f9123f2
Jeff <mx.jeferson.12@gmail.com>
2021-11-08 13:22:35 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from scrapper_boilerplate.setup import setSelenium
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def main():
+    """
+    Insert your code here
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
